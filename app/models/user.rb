662ae3e7305031e5ecf407538e4072540c1259be
Tsiory-prdx <tsiory.rktmhnn@gmail.com>
2019-12-05 16:07:07 +0300

    DB update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
  #relation
-belongs_to :user
+
+has_one :cart
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
