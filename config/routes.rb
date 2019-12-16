1343aa5b986bc025dea9ffd0dd45c2ead63672ff
Manolotsoa1506 <manolotsoadaniel@gmail.com>
2019-12-16 13:09:30 +0300

    update branch admin_site

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'admin_site/index'
   get 'home/index'
   get 'test/index'
+  get 'test/show_restaurant'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'connection/index'
   get 'editprofil/info'
