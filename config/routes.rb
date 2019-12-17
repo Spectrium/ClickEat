d8fb53680ae220d617416071eaf43bb059021e93
Tsiory-prdx <tsiory.rktmhnn@gmail.com>
2019-12-17 11:07:53 +0300

    routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'test/index'
-  get 'test/show_restaurant'
-  get "restaurant/index"
-  get "dish/index"
+  resources :test
+  resources :restaurant
+  resources :dish
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
