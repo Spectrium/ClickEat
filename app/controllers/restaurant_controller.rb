501d7977b2dfaea0903bf01a25c6dd0414580045
Tsiory-prdx <tsiory.rktmhnn@gmail.com>
2019-12-18 11:47:52 +0300

    need help

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -2,4 +2,9 @@ class RestaurantController < ApplicationController
     def index
          @restaurants = Restaurant.all
     end
+
+    def show
+        @restaurant = Restaurant.find(params[:id])
+        @dishes = @restaurant.dishes
+    end
 end
