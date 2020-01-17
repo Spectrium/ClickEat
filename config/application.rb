35809580035c2842d401f05994a692dc973374bd
ismael-i <ismaelrazafindramboly@gmail.com>
2020-01-17 14:04:43 +0300

    view devise modif

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Clickeat
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.paths << Rails.root.join("app", "javascript","packs","images");
+    config.i18n.default_locale = :en
   end
 end
