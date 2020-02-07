2f32d47706522aaf1aafd81d889650099174ddce
ismael-i <ismaelrazafindramboly@gmail.com>
2020-02-07 12:36:02 +0300

    first ajout apli

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,9 +260,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_KEY']
-  config.omniauth :google_oauth2, ENV['GMAIL_LOGIN'], ENV['GMAIL_PWD'],
-  callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_KEY'],callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :google_oauth2, ENV['GMAIL_SIGN'], ENV['GMAIL-KEY'], callback_url: "http://localhost:3000/users/auth/google_oauth2/callback"
+  
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
